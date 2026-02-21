9d339c4510b2f953a9e454fb1c4deb75b21a1e75
Esaius2058 <wabwirewild@gmail.com>
2026-02-21 13:41:59 +0300

    feat: update dependencies

--- frontend-web/src/main.tsx
+++ frontend-web/src/main.tsx
@@ -1,5 +1,20 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import MainRoutes from './routes.tsx';
+import './index.css';
+import { Toaster } from 'sonner';
 
-createRoot(document.getElementById("root")!).render(<App />);
+const router = createBrowserRouter(MainRoutes());
+const root = document.getElementById('root') as HTMLElement;
+
+if (!root){
+  throw new Error("Root element not found");
+}
+
+createRoot(root).render(
+  <StrictMode>
+      <Toaster position='top-right' richColors />
+      <RouterProvider router={router} />
+  </StrictMode>,
+)
